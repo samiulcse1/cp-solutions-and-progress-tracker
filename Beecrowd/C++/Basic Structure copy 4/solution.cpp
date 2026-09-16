7f6745f7617a14bd9f493f2db27f8ba59de23abf
𝙼𝙳. 𝚂𝙰𝙼𝙸𝚄𝙻 𝙸𝚂𝙻𝙰𝙼 <samiulcse1@gmail.com>
2026-09-16 23:55:18 +0600

    Update solution.cpp

--- Beecrowd/C++/Basic Structure copy 4/solution.cpp
+++ Beecrowd/C++/Basic Structure copy 4/solution.cpp
@@ -4,19 +4,5 @@ using namespace std;
 int main()
 {
 
-  int size;
-  cin >> size;
-  vector<pair<int, int>> vec(size);
-
-  for (int i = 0; i < vec.size(); i++)
-  {
-
-    vec[i].second = i;
-    cin >> vec[i].first;
-  }
-
-  sort(vec.begin(), vec.end());
-  cout << vec[0].first + 1 << endl;
-
-    return 0;
+  return 0;
 }
