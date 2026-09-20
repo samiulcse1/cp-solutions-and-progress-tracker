ff9a77f2b5bb0ac8a6c79a403d7d66eb4094afaf
MD. SAMIUL ISLAM <251-15-796@diu.edu.bd>
2026-09-20 15:12:31 +0600

    added

--- Beecrowd/C++/Basic Structure copy 6/solution.cpp
+++ Beecrowd/C++/Basic Structure copy 6/solution.cpp
@@ -3,13 +3,30 @@ using namespace std;
 
 int main()
 {
-
-
-
-
-    
-
-
+    int t = 3;
+    int result = 0;
+    while (t)
+    {
+        string cc;
+        getline(cin, cc);
+        if (cc != "caw caw")
+        {
+            int base = 2;
+            for (int i = 2; i >= 0; i--)
+            {
+                if (cc[2 - i] == '*')
+                {
+                    result += pow(base, i);
+                }
+            }
+        }
+        else
+        {
+            cout << result << endl;
+            t--;
+            result = 0;
+        }
+    }
 
     return 0;
 }
