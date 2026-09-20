8332c703c85dda2cb27b3c515648229a390457c9
MD. SAMIUL ISLAM <251-15-796@diu.edu.bd>
2026-09-20 17:13:21 +0600

    done

--- Beecrowd/C++/Basic Structure copy 4/solution.cpp
+++ Beecrowd/C++/Basic Structure copy 4/solution.cpp
@@ -8,6 +8,16 @@ int main()
     cin >> n;
     while (n--)
     {
+        string p1, c1, p2, c2;
+        long long n, m;
+
+        cin >> p1 >> c1 >> p2 >> c2;
+        cin >> n >> m;
+
+        if ((n + m) % 2 == 0)
+            cout << (c1 == "PAR" ? p1 : p2) << endl;
+        else
+            cout << (c1 == "IMPAR" ? p1 : p2) << endl;
     }
 
     return 0;
