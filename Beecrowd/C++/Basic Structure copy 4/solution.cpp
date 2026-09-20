0283a872ea163cf5fb91345774c3c6fba142ab19
MD. SAMIUL ISLAM <251-15-796@diu.edu.bd>
2026-09-20 17:01:38 +0600

    solved

--- Beecrowd/C++/Basic Structure copy 4/solution.cpp
+++ Beecrowd/C++/Basic Structure copy 4/solution.cpp
@@ -4,12 +4,24 @@ using namespace std;
 int main()
 {
 
+    int n;
+    cin >> n;
+    while (n--)
+    {
 
+        string name;
+        int force;
+        cin >> name >> force;
 
-
-    
-
-
+        if (name == "Thor")
+        {
+            cout << "Y" << endl;
+        }
+        else
+        {
+            cout << "N" << endl;
+        }
+    }
 
     return 0;
 }
