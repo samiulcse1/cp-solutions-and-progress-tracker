37572b84312c4c98a49f5739afb97c47670fa7d1
MD. SAMIUL ISLAM <251-15-796@diu.edu.bd>
2026-09-20 16:54:53 +0600

    solved

---
+++ Beecrowd/C++/1864 - Our Days Are Never Coming Back/solution.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    string s = "LIFE IS NOT A PROBLEM TO BE SOLVED BUT A REALITY TO BE EXPERIENCED";
+
+    for (int i = 0; i < n; i++)
+        cout << s[i];
+
+    cout << endl;
+
+    return 0;
+}
