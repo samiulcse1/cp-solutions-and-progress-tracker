d26995b4db09f1372394971e1c98684b6b46deba
MD. SAMIUL ISLAM <251-15-796@diu.edu.bd>
2026-09-21 21:13:19 +0600

    solving

---
+++ Codeforces/Contest/Codeforces Round 1122 (Div. 3)/B. Three Piles/solution.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        vector<long long> a(3);
+        for (int i = 0; i < 3; i++)
+        {
+            cin >> a[i];
+        }
+    }
+
+    return 0;
+}
