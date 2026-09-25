0482b6a263075af6eb93ec5a60cfe26ae1b60962
MD. SAMIUL ISLAM <251-15-796@diu.edu.bd>
2026-09-25 21:01:42 +0600

    Update solution.cpp

--- Codeforces/Contest/Codeforces Round 1123 (Div. 2)/A. Good Contest/solution.cpp
+++ Codeforces/Contest/Codeforces Round 1123 (Div. 2)/A. Good Contest/solution.cpp
@@ -3,20 +3,6 @@ using namespace std;
 
 int main()
 {
-    int t, value;
-    cin >> t;
-
-    while (t--)
-    {
-        cin >> value;
-        vector<int> a(3);
-        for (int i = 0; i < 3; i++)
-        {
-            cin >> a[i];
-        }
-        sort(a.begin(), a.end());
-        cout << value - (a[0]) << endl;
-    }
 
     return 0;
 }
